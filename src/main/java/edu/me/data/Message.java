3527c61930236c57eeee48cf0780216a1c1001de
freddy <freddythetrout@gmx.de>
2023-09-17 13:09:35 +0200

    Initial Implementation of SenderSocketManager

--- src/main/java/edu/me/data/Message.java
+++ src/main/java/edu/me/data/Message.java
@@ -1,5 +1,6 @@
 package edu.me.data;
 
+import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
@@ -7,12 +8,13 @@ import java.time.LocalDateTime;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Message {
+public class Message implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private String messageText;
 	private LocalDateTime time;
 	private String sender;
-	private final  Logger logger = LogManager.getLogger(Message.class);
+	private final Logger logger = LogManager.getLogger(Message.class);
 
 	public Message(String messageText) {
 		this.messageText = messageText;
--- src/main/java/edu/me/network/SenderSocketManager.java
+++ src/main/java/edu/me/network/SenderSocketManager.java
@@ -1,5 +1,65 @@
 package edu.me.network;
 
-public class SenderSocketManager {
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import edu.me.data.Message;
+
+public class SenderSocketManager implements Runnable {
+
+	private static final Logger logger = LogManager.getLogger(RecieverSocketManager.class);
+
+	private Message message;
+	private Integer port;
+	private String ip;
+	private ObjectOutputStream objectOutputStream;
+
+	public SenderSocketManager(Message message, String ip, Integer port) {
+		this.message = message;
+		this.port = port;
+		this.ip = ip;
+	}
+
+	private void createOutputStreamToSocket() {
+		Socket senderSocket;
+		ObjectOutputStream oos = null;
+		try {
+			senderSocket = new Socket(ip, port);
+			oos = new ObjectOutputStream(senderSocket.getOutputStream());
+		} catch (IOException e) {
+			logger.error("Could not create socker or objectoutputstream", e);
+			e.printStackTrace();
+		}
+		this.objectOutputStream = oos;
+	}
+
+	private void sendMessage() {
+		try {
+			if (this.objectOutputStream == null) {
+				throw new IllegalStateException();
+			}
+		} catch (Exception e) {
+			logger.error("An ObjectOutPutStream has to be created using sendMessage() before invoking this method", e);
+		}
+
+		try {
+			this.objectOutputStream.writeObject(message);
+		} catch (IOException e) {
+			logger.error("Message could not be sent by the OutPutStream", e);
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void run() {
+		createOutputStreamToSocket();
+		sendMessage();
+
+	}
 
 }
