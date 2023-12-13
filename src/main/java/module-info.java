8b8ed03a6d8884ed988f0aec1a8604206a2337bb
freddy <freddythetrout@gmx.de>
2023-12-13 10:49:58 +0100

    Set up module-info.java correcty

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,14 @@
 module PeerChatApp {
 
-	exports edu.me.main to javafx.graphics;
-	exports edu.me.view;
+	exports edu.me.main to javafx.fxml, javafx.graphics;
+	exports edu.me.view to javafx.fxml;
+
+	opens edu.me.view to javafx.fxml;
 
 	requires javafx.base;
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires javafx.controls;
 	requires org.apache.logging.log4j;
-
-	opens edu.me.main to javafx.fxml;
-	opens edu.me.view to javafx.fmxl;
 
 }
